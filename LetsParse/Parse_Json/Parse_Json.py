b06672d8e17af9a48202c4d96d15831ed10a8012
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-02 21:05:57 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -1,19 +1,20 @@
 import json
 import pandas as pd
-from operator import getitem
 
+# with open('E:\\01_Work\\Python\\LetsParse\\Parse_Json\\EventSound.json') as json_file:
+#     json_data = json.load(json_file)
+#
+#     json_str = json.dumps(json_data, indent=4)
+#     json_dict = json.loads(json_str)
+#
+#     print(json_dict)
+#     print(json_dict['datas'])
+#     print(len(json_dict['datas']))
+#
+#     for i in range(len(json_dict['datas'])):
+#          for k in json_dict['datas'][i].keys(): # k = theme name
+#              print(k)
 
+df = pd.read_json("./EventSound.json")
 
-with open('E:\\01_Work\\Python\\Parse_SpoonzeSoundJson\\EventSound.json') as json_file:
-    json_data = json.load(json_file)
-
-    json_str = json.dumps(json_data, indent=4)
-    json_dict = json.loads(json_str)
-
-    print(json_dict['datas'])
-    print(len(json_dict['datas']))
-    #
-    # for i in range(len(json_dict['datas'])):
-    #     for k in json_dict['datas'][i].keys(): # k = theme name
-    #         print(k)
-
+https://youtu.be/SrQlQrfcFwY
