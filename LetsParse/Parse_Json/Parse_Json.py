d39592712fe181fcfd5acbe475629e4f7f4db6b4
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-03 00:39:13 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -1,23 +1,41 @@
 import json
-import pandas as pd
+import pprint
 
-# with open('E:\\01_Work\\Python\\LetsParse\\Parse_Json\\EventSound.json') as json_file:
-#     json_data = json.load(json_file)
-#
-#     json_str = json.dumps(json_data, indent=4)
-#     json_dict = json.loads(json_str)
-#
-#     print(json_dict)
-#     print(json_dict['datas'])
-#     print(len(json_dict['datas']))
-#
-#     for i in range(len(json_dict['datas'])):
-#          for k in json_dict['datas'][i].keys(): # k = theme name
-#              print(k)
+pp = pprint.PrettyPrinter(indent=4)
 
-df = pd.read_json("./EventSound.json")
+with open('./EventSound.json') as json_file:
+    json_data = json.load(json_file)
 
-# https://youtu.be/SrQlQrfcFwY
+    json_str = json.dumps(json_data, indent=4)
+    json_dict = json.loads(json_str)
+    print(json_str)
+    #pp.pprint(json_dict)
+    print(type(json_dict['datas'])) # list
+    print(json_dict['datas'][0]) # {'Bt': [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]}
+    print(type(json_dict['datas'][0])) # dict
+    print(json_dict['datas'][0]['Bt']) # [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]
+    print(type(json_dict['datas'][0]['Bt'])) #list
+    print(json_dict['datas'][0]['Bt'][0]) # {'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}
+    print(type(json_dict['datas'][0]['Bt'][0])) #dict
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk']) #[{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]
+    print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'])) # list
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]) # {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}
+    print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]))  # dict
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['animName'])
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['soundName'])
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['targetObjName'])
+    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['sequenceTime'])
 
-#   datas - bt - home_bt_moving_walk - animname/sequncetime/soundname ...
-#   datas - theme - animname - ...
+    for i in range(len(json_dict['datas'])):
+         for k in json_dict['datas'][i].keys(): # k = theme name
+             print(k)
+
+    for i in range(len(json_dict['datas'][0]['Bt'][0])):
+         for k in json_dict['datas'][0]['Bt'][i].keys(): # k = theme name
+             print(k)
+
+    for i in range(len(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0])):
+         for k in json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][i].keys(): # k = theme name
+             print(k)
+
+    #https://khanrc.tistory.com/entry/PyCon-%EC%9C%84%EB%8C%80%ED%95%9C-dict-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B3%A0-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
