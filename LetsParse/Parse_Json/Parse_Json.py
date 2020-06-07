17ff1a96c1ee9dbff7da86ba00ee099b39dbf6e3
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-08 00:29:51 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -1,41 +1,63 @@
 import json
+from json2xls.json2xls import Json2Xls
 import pprint
+from json2xml import json2xml
+from json2xml.utils import readfromjson
 
 pp = pprint.PrettyPrinter(indent=4)
 
+data = readfromjson('./EventSound.json')
+#print(json2xml.Json2xml(data).to_xml())
+
 with open('./EventSound.json') as json_file:
     json_data = json.load(json_file)
-
     json_str = json.dumps(json_data, indent=4)
     json_dict = json.loads(json_str)
-    print(json_str)
-    #pp.pprint(json_dict)
-    print(type(json_dict['datas'])) # list
-    print(json_dict['datas'][0]) # {'Bt': [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]}
-    print(type(json_dict['datas'][0])) # dict
-    print(json_dict['datas'][0]['Bt']) # [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]
-    print(type(json_dict['datas'][0]['Bt'])) #list
-    print(json_dict['datas'][0]['Bt'][0]) # {'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}
-    print(type(json_dict['datas'][0]['Bt'][0])) #dict
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk']) #[{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]
-    print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'])) # list
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]) # {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}
-    print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]))  # dict
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['animName'])
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['soundName'])
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['targetObjName'])
-    print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['sequenceTime'])
+    obj = Json2Xls('test.xls', json_str)
+    obj.make()
+
+    edges = []
+    leaves = []
+    nodes = []
+
+
+    def paths(data):
+        for key, value in data.items():
+            yield key
+            if 'children' in value:
+                for child in value['children']:
+                    for path in paths(child):
+                        yield f'{key}/{path}'
+
+
+    print(list(paths(json_dict['datas'][0])))
 
     for i in range(len(json_dict['datas'])):
-         for k in json_dict['datas'][i].keys(): # k = theme name
-             print(k)
+        print(list(paths(json_dict['datas'][i])))
 
-    for i in range(len(json_dict['datas'][0]['Bt'][0])):
-         for k in json_dict['datas'][0]['Bt'][i].keys(): # k = theme name
-             print(k)
 
-    for i in range(len(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0])):
-         for k in json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][i].keys(): # k = theme name
-             print(k)
-
-    #https://khanrc.tistory.com/entry/PyCon-%EC%9C%84%EB%8C%80%ED%95%9C-dict-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B3%A0-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
+    # # print(json_str)
+    # pp.pprint(json_dict)
+    # print(type(json_dict['datas'])) # list
+    # print(json_dict['datas'][0]) # {'Bt': [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]}
+    # print(type(json_dict['datas'][0])) # dict
+    # print(json_dict['datas'][0]['Bt']) # [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]
+    # print(type(json_dict['datas'][0]['Bt'])) #list
+    # print(json_dict['datas'][0]['Bt'][0]) # {'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}
+    # print(type(json_dict['datas'][0]['Bt'][0])) #dict
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk']) #[{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]
+    # print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'])) # list
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]) # {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}
+    # print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]))  # dict
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['animName'])
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['soundName'])
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['targetObjName'])
+    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['sequenceTime'])
+    #
+    # for i in range(len(json_dict['datas'])):
+    #      for k in json_dict['datas'][i].keys(): # k = theme name
+    #          print(k)
+    #
+    # for i in range(len(json_dict['datas'][0]['Bt'][0])):
+    #      for k in json_dict['datas'][0]['Bt'][i].keys(): # k = theme name
+    #          print(k)
