9c1f01986ac519a1d2f8f5e1e4f5d2ec62e59981
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-09 23:13:29 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -1,56 +1,25 @@
 import json
-from json2xls.json2xls import Json2Xls
-import pprint
-from json2xml import json2xml
-from json2xml.utils import readfromjson
-
-pp = pprint.PrettyPrinter(indent=4)
-
-data = readfromjson('./EventSound.json')
-#print(json2xml.Json2xml(data).to_xml())
 
 with open('./EventSound.json') as json_file:
     json_data = json.load(json_file)
-    json_str = json.dumps(json_data, indent=4)
+    json_str = json.dumps(json_data)
     json_dict = json.loads(json_str)
-    obj = Json2Xls('test.xls', json_str)
-    obj.make()
 
-    def paths(data):
-        for key, value in data.items():
-            yield key
-            if 'children' in value:
-                for child in value['children']:
-                    for path in paths(child):
-                        yield f'{key}/{path}'
+notifies = []
 
+for i in range(len(json_dict['datas'])):
+     for k in json_dict['datas'][i].keys():
+         for n in json_dict['datas'][i][k][0].keys():
+            for r in range(len(json_dict['datas'][i][k][0][n])):
+                dict = json_dict['datas'][i][k][0][n][r]
+                dict['theme'] = k
+                notifies.append(dict)
 
-    for i in range(len(json_dict['datas'])):
-        print(list(paths(json_dict['datas'][i])))
+print(notifies)
 
-
-    # # print(json_str)
-    # pp.pprint(json_dict)
-    # print(type(json_dict['datas'])) # list
-    # print(json_dict['datas'][0]) # {'Bt': [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]}
-    # print(type(json_dict['datas'][0])) # dict
-    # print(json_dict['datas'][0]['Bt']) # [{'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}]
-    # print(type(json_dict['datas'][0]['Bt'])) #list
-    # print(json_dict['datas'][0]['Bt'][0]) # {'home_bt_moving_walk': [{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]}
-    # print(type(json_dict['datas'][0]['Bt'][0])) #dict
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk']) #[{'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}, {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '50.1'}]
-    # print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'])) # list
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]) # {'animName': 'home_bt_moving_walk', 'soundName': 'BT_Footstep', 'targetObjName': '', 'sequenceTime': '0.1'}
-    # print(type(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]))  # dict
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['animName'])
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['soundName'])
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['targetObjName'])
-    # print(json_dict['datas'][0]['Bt'][0]['home_bt_moving_walk'][0]['sequenceTime'])
-    #
-    # for i in range(len(json_dict['datas'])):
-    #      for k in json_dict['datas'][i].keys(): # k = theme name
-    #          print(k)
-    #
-    # for i in range(len(json_dict['datas'][0]['Bt'][0])):
-    #      for k in json_dict['datas'][0]['Bt'][i].keys(): # k = theme name
-    #          print(k)
+for dicts in notifies:
+    print(dicts['theme'])
+    print(dicts['animName'])
+    print(dicts['soundName'])
+    print(dicts['targetObjName'])
+    print(dicts['sequenceTime'])
