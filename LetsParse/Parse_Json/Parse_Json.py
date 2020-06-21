94c23e9e53be6b8a9394efd6c430577f67fdcb2c
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-21 18:15:27 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -1,25 +1,47 @@
 import json
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+ws.title = 'EventSound'
 
 with open('./EventSound.json') as json_file:
     json_data = json.load(json_file)
     json_str = json.dumps(json_data)
     json_dict = json.loads(json_str)
 
-notifies = []
+all = []
 
 for i in range(len(json_dict['datas'])):
-     for k in json_dict['datas'][i].keys():
-         for n in json_dict['datas'][i][k][0].keys():
+    for k in json_dict['datas'][i].keys():  # k = theme, 캐릭터 이름
+        dict = {}
+        dict['Theme Or Character'] = k
+        for n in json_dict['datas'][i][k][0].keys():  # n = 애니메인션 이름
             for r in range(len(json_dict['datas'][i][k][0][n])):
-                dict = json_dict['datas'][i][k][0][n][r]
-                dict['theme'] = k
-                notifies.append(dict)
+                dict.update(json_dict['datas'][i][k][0][n][r])  # dict = 노티파이 딕셔너
+                all.append(dict)
 
-print(notifies)
+def toxls(all):
 
-for dicts in notifies:
-    print(dicts['theme'])
-    print(dicts['animName'])
-    print(dicts['soundName'])
-    print(dicts['targetObjName'])
-    print(dicts['sequenceTime'])
+    rowindex = 1
+    colindex = 1
+
+    for key in all[0]:
+        ws.cell(row=rowindex, column=colindex).value = key
+        colindex += 1
+
+    colindex = 1
+    rowindex = 2
+
+    for dict in all:
+        for data in dict:
+            ws.cell(row=rowindex, column=colindex).value = dict[data]
+            colindex += 1
+        rowindex += 1
+        colindex = 1
+
+
+toxls(all)
+wb.save('./EventSound.xlsx')
+wb.close()
+print('done')
