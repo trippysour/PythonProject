c39d9fbfd8cc622592fbcba9cc54cc8509044b51
trippysour <55823151+trippysour@users.noreply.github.com>
2020-06-26 17:12:43 +0900

    Update Parse_Json.py

--- LetsParse/Parse_Json/Parse_Json.py
+++ LetsParse/Parse_Json/Parse_Json.py
@@ -18,13 +18,12 @@ for i in range(len(json_dict['datas'])):
         dict1 = {}
         dict1['Theme Or Character'] = k
         for s in range(len(json_dict['datas'][i][k])):
-            print(json_dict['datas'][i][k][s])
-            for n in json_dict['datas'][i][k][0].keys():  # n = 애니메인션 이름
-                for r in range(len(json_dict['datas'][i][k][0][n])):
-                    dict2 = json_dict['datas'][i][k][0][n][r]
+            for n in json_dict['datas'][i][k][s].keys():  # n = 애니메인션 이름
+                for r in range(len(json_dict['datas'][i][k][s][n])):
+                    dict2 = json_dict['datas'][i][k][s][n][r]
                     dict3 = {**dict1, **dict2} # 딕셔너리 더하기
                     all.append(dict3)
-print(all)
+# print(all)
 '''
 엑셀로 쓰는 함수, 첫번째 행은 key들을 넣어주고 두번째 행부터는 value들
 '''
@@ -72,18 +71,18 @@ wb.close()
 
 '''
 GUI 코드
-'''
-from PySide2.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QCheckBox, QMessageBox, QApplication
-
-
-class Form(QWidget):
-    def __init__(self):
-        super(Form, self).__init__()
-        self.setWindowTitle("Wwise TSV Importer")
-        self.setMinimumSize(350, 200)
-        self.setMaximumSize(350, 200)
-
-
-
-app = QApplication([])
-GUI = Form()
+# '''
+# from PySide2.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QCheckBox, QMessageBox, QApplication
+#
+#
+# class Form(QWidget):
+#     def __init__(self):
+#         super(Form, self).__init__()
+#         self.setWindowTitle("Wwise TSV Importer")
+#         self.setMinimumSize(350, 200)
+#         self.setMaximumSize(350, 200)
+#
+#
+#
+# app = QApplication([])
+# GUI = Form()
