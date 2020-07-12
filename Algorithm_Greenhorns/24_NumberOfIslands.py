3b6d37291ddfc22ba6b58f47579af4a298c3eb26
trippysour <55823151+trippysour@users.noreply.github.com>
2020-07-13 00:58:39 +0900

    Create 24_NumberOfIslands.py

---
+++ Algorithm_Greenhorns/24_NumberOfIslands.py
@@ -0,0 +1,23 @@
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+
+def numIslands(grid):
+
+    count = 0
+    queue = []
+    visited = []
+
+    queue.append([0,0])
+    visited.append([0,0])
+
+    while queue
+
+
+numIslands(grid)
+
+
+#https://hanxxang.tistory.com/70
