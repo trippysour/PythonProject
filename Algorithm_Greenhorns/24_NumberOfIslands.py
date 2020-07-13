448c39d50de7f01e9781e36ee3ecbc0a17eb98b3
trippysour <55823151+trippysour@users.noreply.github.com>
2020-07-14 00:00:33 +0900

    Update 24_NumberOfIslands.py

--- Algorithm_Greenhorns/24_NumberOfIslands.py
+++ Algorithm_Greenhorns/24_NumberOfIslands.py
@@ -11,13 +11,19 @@ def numIslands(grid):
     queue = []
     visited = []
 
-    queue.append([0,0])
-    visited.append([0,0])
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            #visited.append([i, j])
+            #print (grid[i][j])
+            if grid[i][j] == '1' and grid[i][j] not in visited: # 이것 뿐만 아니라 상하좌우도 체크
+                count += 1
+                print('yes')
 
-    while queue
+
+
+# 하나씩 방문하면서 값이 1인지 체크
+# 값이 1이고 visited에 속하지 않으면서 visited의 상하좌우에 속하지 않으면 count + 1
 
 
 numIslands(grid)
 
-
-#https://hanxxang.tistory.com/70
