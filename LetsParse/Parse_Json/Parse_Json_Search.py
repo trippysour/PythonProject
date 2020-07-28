67b6d54d926f625c86a73e723ee39666c9317cef
trippysour <55823151+trippysour@users.noreply.github.com>
2020-07-28 23:30:22 +0900

    Update and rename Parse_Json_xml.py to Parse_Json_Search.py

---
+++ LetsParse/Parse_Json/Parse_Json_Search.py
@@ -0,0 +1,14 @@
+import json
+from jsonpath_ng import jsonpath, parse
+
+
+with open('EventSound.json') as j:
+    d = json.load(j)
+    json_str = json.dumps(d)
+    json_dict = json.loads(json_str)
+
+    jsonpath_expr = parse('$..soundName')
+
+    for match in jsonpath_expr.find(json_dict):
+        print(match.value)
+        print(match.full_path)
